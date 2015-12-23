589998a048945f27312cd64a900674124e6e43ef
Gabe Hernandez <gabriel.hernandez@lab49.com>
2015-12-23 17:29:48 +0000

    configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  
-}
+  context: __dirname,
+  entry: './index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+
+  loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel?presets[]=react,presets[]=es2015'
+    }
+  ]
+};
