237b0b270e2cba7326ee3308d8972583fda9b567
Gabe Hernandez <gabriel.hernandez@lab49.com>
2015-12-24 13:54:34 +0000

    configured webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,50 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var PATHS = {
+ app: path.join(__dirname, 'app'),
+ build: path.join(__dirname, 'build')
+};
+
 module.exports = {
   context: __dirname,
-  entry: './index.js',
+  entry: PATHS.app,
   output: {
-    path: __dirname + '/dist',
+    path: PATHS.build,
     filename: 'bundle.js'
   },
 
+  // loaders
   loaders: [
     {
       test: /\.jsx?$/,
       exclude: /(node_modules|bower_components)/,
       loader: 'babel?presets[]=react,presets[]=es2015'
     }
+  ],
+
+  // dev server options
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+
+    // Display only errors to reduce the amount of output.
+    stats: 'errors-only',
+
+    // Parse host and port from env so this is easy to customize.
+    host: process.env.HOST,
+    port: process.env.PORT,
+
+  },
+
+  // plugins
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'React Redux TODO App'
+    })
   ]
 };
