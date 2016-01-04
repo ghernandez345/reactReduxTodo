557947061794423ee9b78f6b7349ed6ddfc4abee
Gabe Hernandez <gabriel.hernandez@lab49.com>
2016-01-04 14:12:02 +0000

    added actions

--- app/actions.js
+++ app/actions.js
@@ -15,3 +15,31 @@ export const VisibilityFilter = {
   SHOW_COMPLETED: 'SHOW_COMPLETED',
   SHOW_ACTIVE: 'SHOW_ACTIVE'
 };
+
+/**
+ * action creators
+ */
+
+let nextTodoId = 0;
+
+export function addTodo(text) {
+  return {
+    type: ADD_TODO,
+    id: nextTodoId++,
+    text
+  };
+}
+
+export function completeTodo(id) {
+  return {
+    type: COMPLETE_TODO,
+    id
+  };
+}
+
+export function setVisibilityFilter(filter) {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter
+  };
+}
