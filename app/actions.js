00f20d1d580e9533f6068186819948c198e20973
Gabe Hernandez <gabriel.hernandez@lab49.com>
2016-01-04 10:49:55 +0000

    add eslint support

---
+++ app/actions.js
@@ -0,0 +1,17 @@
+/**
+ * Actions types
+ */
+
+export const ADD_TODO = 'ADD_TODO'
+export const COMPLETE_TODO = 'COMPLETE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+/**
+ * other constants
+ */
+
+export const VisibilityFilter = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,18 @@ module.exports = {
     filename: 'bundle.js'
   },
 
-  // loaders
-  loaders: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015']
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['react', 'es2015']
+        }
       }
-    }
-  ],
+    ],
+  },
 
   // dev server options
   devServer: {
